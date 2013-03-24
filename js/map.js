60fe63bc276bb50c90815b6d28737a0debdb8387
Evan J Brunner <ejbrun@appittome.com>
2013-03-24 09:46:14 -0700

    Organized for progressions
    Implemented callback structure that moves
    from map to dialog.  This needs to be changed.
    
      ADDED: progressions map->dialog

--- js/map.js
+++ js/map.js
@@ -4,17 +4,23 @@ var Oracle = {};
 Oracle.Map = new function() {
   var my = {};
 
-  $(document).on("click", ".agent", function() {
-    alert("clicked on agent " + $(this).data("agent-id"));
-  });
-  
-  my.placeAgent = function(agent_id, image_path, x, y) {
+  my.place = function(callback) {
+      var rand_x = 0, rand_y = 0;
+      $.each(Oracle.agents, function(i,img) {
+	//max x= 530px max_y = 240
+	rand_x = 10; rand_y = 10;
+        rand_x += Math.ceil(Math.random() * 520);
+        rand_y += Math.ceil(Math.random() * 230);
+	my.placeAgent(i, callback, img, rand_x, rand_y);});
+  };
+  my.placeAgent = function(agent_id, callback, image_path, x, y) {
     $("<img>").
       attr("src", "images/" + image_path).
       data("agent-id", agent_id).
       addClass("agent").
       css("left", x).
       css("top", y).
+      on("click", callback).
       appendTo($("#canvas"));
   };
   
